7857976fa5d2c152536513293eb3a015d1cb4e5f
wenchao.gao <kewill123>
2017-08-23 17:13:33 +0800

    modify server port move package

---
+++ common/src/main/java/com/posidon/common/base/CommonApplication.java
@@ -0,0 +1,16 @@
+package com.posidon.common.base;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class CommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommonApplication.class, args);
+	}
+
+
+}
